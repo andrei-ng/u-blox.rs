f1c6c2ce0127083e27a5e3b82bdbcab5390c1c40
Andrei Gherghescu <8067229+andrei-ng@users.noreply.github.com>
2025-02-04 20:56:32 +0100

    fix nav-pvt flags3 differences with series9
     - add compile error if no ubx series feature is enabled, by example
       when `--no-default-features` is provided
    
    Signed-off-by: Andrei Gherghescu <8067229+andrei-ng@users.noreply.github.com>

--- ublox/build.rs
+++ ublox/build.rs
@@ -3,4 +3,9 @@ fn main() {
     compile_error!(
         r#"The "ubx_series8" and "ubx_series9" features are mutually exclusive and cannot be activated at the same time. Please disable one or the other."#
     );
+
+    #[cfg(all(not(feature = "ubx_series8"), not(feature = "ubx_series9")))]
+    compile_error!(
+        r#"At least one feature "ubx_series8" or "ubx_series9" has to be selected. Please select only one."#
+    );
 }
